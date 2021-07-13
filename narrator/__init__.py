81681a3e490333c74fc9b3222582ae6401e35fef
Georg <krysopath@gmail.com>
2021-07-13 02:10:17 +0200

    cleanup

--- narrator/__init__.py
+++ narrator/__init__.py
@@ -2,59 +2,8 @@ from random import randint, choice
 from time import sleep
 from dataclasses import dataclass
 
-from .gen import Avatar, Question, Choice
-from .events import Event
-
-prophet_allfather = {"follower": 10}
-prophet_presidalil = {"food": 5, "follower": 5}
-prophet_wenom = {"follower": 5, "money": 5}
-prophet_alkor = {"follower": 5, "health": -5}
-prophet_haltarna = {"follower": 5, "money": 5}
-
-DIFFICULTY = 1
-
-
-def reward(fl, fo, mo, hp):
-    return {
-        "food": fo*DIFFICULTY,
-        "follower": fl*DIFFICULTY,
-        "money": mo*DIFFICULTY,
-        "health": hp*DIFFICULTY
-    }
-
-story = [
-    Event(
-        "Which god will you serve?",
-        {},
-        [
-            Choice("Allvater, god of the skies and stars", prophet_allfather),
-            Choice("Presidalil, god of life and growth", prophet_presidalil),
-            Choice("Wenom, god of death", prophet_wenom),
-            Choice("Alkor, god of love and insanity", prophet_alkor),
-            Choice("Hal'tarna, god of materia", prophet_haltarna),
-        ],
-    ),
-    Event(
-        "Would be a force of Evil or of Good?",
-        {},
-        [
-            Choice("I dont like either", reward(0, 0, 5, 10)),
-            Choice("I like good", reward(10, 0, 0, 0)),
-            Choice("I like Evil!", reward(0, 0, 0, 10)),
-        ],
-    ),
-    Event(
-        "Why do you serve?",
-        {},
-        [
-            Choice("i want to be rich!", reward(0, 5, 15, 0)),
-            Choice("I live to serve my god", reward(15, 0, 0, 5)),
-            Choice("wanderlust", reward(0, 10, 5, 5)),
-            Choice("for might and glory", reward(10, 0, 4, 4)),
-        ],
-    ),
-]
-
+from .gen import Avatar
+from .events import story, story_map
 
 
 @dataclass
@@ -78,6 +27,7 @@ def update_values(game, values):
 
 
 def run():
+    print(story_map)
     name = input("Enter your name, prophet: ")
     player = create(
             {
@@ -99,7 +49,5 @@ def run():
 
         chosen = int(input(f"{q}\n\nYour choice, {name}? "))
         update_values(game, q.choices[chosen].values)
-
-        # print("#", end="", flush=True)
 
     print(game.player, game.values)
--- narrator/events.py
+++ narrator/events.py
@@ -1,10 +1,29 @@
 import typing as t
 from dataclasses import dataclass, asdict
-from .gen import Choice
+
+
+@dataclass
+class Choice:
+    text: str
+    values: dict
+
+    def __str__(self):
+        value = ""
+        if 'health' in self.values:
+            value += f"{self.values['health']}♥ "
+        if 'follower' in self.values:
+            value += f"{self.values['follower']}🙏 "
+        if 'food' in self.values:
+            value += f"{self.values['food']}🍎 "
+        if 'money' in self.values:
+            value += f"{self.values['money']}💰 "
+
+        return f"{self.text} \n\t{value}"
 
 
 @dataclass
 class Event:
+    id: str
     event_str: str
     data: dict
     choices: t.List[Choice]
@@ -16,6 +35,25 @@ class Event:
             choices += f"\n{i}: {c}"
 
         return f"{desc}\n{choices}"
+
+
+prophet_allfather = {"follower": 10}
+prophet_presidalil = {"food": 5, "follower": 5}
+prophet_wenom = {"follower": 5, "money": 5}
+prophet_alkor = {"follower": 5, "health": -5}
+prophet_haltarna = {"follower": 5, "money": 5}
+
+DIFFICULTY = 1
+
+
+def reward(fl, fo, mo, hp):
+    return {
+        "food": fo*DIFFICULTY,
+        "follower": fl*DIFFICULTY,
+        "money": mo*DIFFICULTY,
+        "health": hp*DIFFICULTY
+    }
+
 
     """
 Du schreitest durch die Tore des Tempelhofes. Keine Erinnerungen an die Zeit
@@ -47,3 +85,43 @@ letztes Geschenk an dich.
 Der Alte reicht dir einen Lederbeutel hin. Er sieht reich gefüllt aus. Du.....
 
 """
+
+story = [
+    Event(
+        "1",
+        "Which god will you serve?",
+        {},
+        [
+            Choice("Allvater, god of the skies and stars", prophet_allfather),
+            Choice("Presidalil, god of life and growth", prophet_presidalil),
+            Choice("Wenom, god of death", prophet_wenom),
+            Choice("Alkor, god of love and insanity", prophet_alkor),
+            Choice("Hal'tarna, god of materia", prophet_haltarna),
+        ],
+    ),
+    Event(
+        "2",
+        "Would be a force of Evil or of Good?",
+        {},
+        [
+            Choice("I dont like either", reward(0, 0, 5, 10)),
+            Choice("I like good", reward(10, 0, 0, 0)),
+            Choice("I like Evil!", reward(0, 0, 0, 10)),
+        ],
+    ),
+    Event(
+        "3",
+        "Why do you serve?",
+        {},
+        [
+            Choice("i want to be rich!", reward(0, 5, 15, 0)),
+            Choice("I live to serve my god", reward(15, 0, 0, 5)),
+            Choice("wanderlust", reward(0, 10, 5, 5)),
+            Choice("for might and glory", reward(10, 0, 4, 4)),
+        ],
+    ),
+]
+
+story_map = {s.id: s for s in story}
+
+
